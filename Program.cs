840bc27138a8b9fd349f69cda1438726aa45b611
Алина <trudolubovaalina5@gmail.com>
2025-02-24 20:32:38 +0400

    Сделана практика А 1 урок

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Class Program
+{
+ static void Main(string[] args)
+{
+// Практика А
+//Создайте программу на псевдоязыке, которая объявляет различные переменные разных типов данных (целочисленный, вещественный, строковый и логический) и выводит их значения на экран.
+int age =20;
+Console.WriteLine($"Age: {age}");
+
+
+double pi = 3.14;
+Console.WriteLine($"Age: {pi}");
+
+string name = "John";
+
+bool isMale = false;
+Console.WriteLine($"IsMale: {isMale}");
+}
+}
+namespace ReadySolution;
+
+class Program
+{
+static void Main(string[] args)
+{
+// Практика А
+
+// Создайте программу, которая объявляет различные переменные разных типов данных
+// (целочисленный, вещественный, строковый и логический) и выводит их значения на экран.
+
+int age = 20;
+Console.WriteLine($"Age: {age}");
+
+double pi = 3.14;
+Console.WriteLine($"Pi: {pi}");
+
+string name = "John";
+Console.WriteLine($"Name: {name}");
+
+bool isMale = false;
+Console.WriteLine($"IsMale: {isMale}");
+
+Console.ReadKey();
+}
+}
